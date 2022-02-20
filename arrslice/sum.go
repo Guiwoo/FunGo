96363194a43f977faee093e44b79eab06b399e7f
Guiwoo <park.guiwoo@hotmail.com>
2022-02-20 15:52:06 +0900

    array and slice

--- arrslice/sum.go
+++ arrslice/sum.go
@@ -7,3 +7,25 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+func SumAll(arrs ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range arrs {
+		sums = append(sums, Sum(numbers))
+	}
+	return sums
+}
+
+func SumAllTails(arrs ...[]int) []int {
+	var sums []int
+	for _, arr := range arrs {
+		if len(arr) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := arr[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+	return sums
+}
